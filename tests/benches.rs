78bcf37733f710a2dc479f185b421675edaa35fa
Serbe <serbenv@gmail.com>
2023-02-08 12:07:50 +0300

    add benches

---
+++ tests/benches.rs
@@ -0,0 +1,39 @@
+#![feature(test)]
+
+use std::str::FromStr;
+
+use netc;
+use test::Bencher;
+
+extern crate test;
+
+#[bench]
+fn bench_bytes_insertion_and_remove(b: &mut Bencher) {
+    let mut header = netc::header::Headers::new();
+    let name = "Content-Type";
+    let value = "text/html; charset=utf-8";
+    let h_f = "Content-Type: text/html; charset=utf-8";
+    b.iter(|| {
+        assert_eq!(header.insert(name, value).unwrap(), None);
+        assert_eq!(
+            header.get(name),
+            Some(&netc::header::HeaderFields::from_str(h_f).unwrap())
+        );
+        assert_eq!(
+            header.remove(name),
+            Some(netc::header::HeaderFields::from_str(h_f).unwrap())
+        );
+    });
+}
+
+#[bench]
+fn bench_string_insertion_and_remove(b: &mut Bencher) {
+    let mut headers = netc::headers::Headers::new();
+    let name = "Content-Type";
+    let value = "text/html; charset=utf-8";
+    b.iter(|| {
+        assert_eq!(headers.insert(name, value), None);
+        assert_eq!(headers.get(name), Some(value.to_string()));
+        assert_eq!(headers.remove(name), Some(value.to_string()));
+    });
+}
